e9a4879446e0b201f44071c1da499838d2ce4441
Kayk Mascarenhas <kaykdsg@gmail.com>
2023-12-04 21:31:53 -0300

    :sparkles: feat: create Produto [ controller, module and repository ]

---
+++ src/modules/produto/produto.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Get, Controller, Post } from '@nestjs/common';
+import { ProdutoRepository } from './produto.repository';
+
+@Controller('/produtos')
+export class ProdutoController {
+  constructor(private produtoRepository: ProdutoRepository) {}
+
+  @Get()
+  async listaProdutos() {
+    return this.produtoRepository.listar();
+  }
+
+  @Post()
+  async criaProduto(@Body() dadosDoProduto) {
+    this.produtoRepository.salvar(dadosDoProduto);
+    return { message: 'Produto criado com sucesso!', dadosDoProduto };
+  }
+}
